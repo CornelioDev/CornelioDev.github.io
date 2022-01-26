853616cc881506b7f310ffbefca8f0c5c1d508ab
José Manuel Cornelio <batcornelio@gmail.com>
2022-01-25 20:51:24 -0400

    Dividing data from functions in JS

---
+++ JS/data.js
@@ -0,0 +1,51 @@
+var user_bio = {
+    name: "José Manuel",
+    last_name: "Cornelio",
+    professional_title: "Web Developer",
+    about: "More than 6 years of experience creating amazing websites and web apps as freelancer and employee of great agencies and organizations, even the Dominican government.",
+    birth_date: "08-19-1993",
+    city: "Santo Domingo",
+    country: "Dominican Republic"
+};
+
+var jobs = [
+    {
+        title: 'Best Choice LLC',
+        monts: 14,
+        description: 'Web Developer and Project Manager'
+    },
+    {
+        title: 'Arzobispado de Sto. Dgo',
+        monts: 28,
+        description: 'Web Developer and Multimedia Designer'
+    },
+    {
+        title: 'Proceso.com.do',
+        monts: 10,
+        description: 'Web Developer'
+    },
+    {
+        title: 'DIGEPEP',
+        monts: 16,
+        description: 'WordPress Developer, Front-End Developer and Web Master'
+    }
+]
+
+var skills = [
+    {
+        title: 'HTML',
+        level: 9
+    },
+    {
+        title: 'CSS',
+        level: 7
+    },
+    {
+        title: 'JavaScript',
+        level: 8
+    },
+    {
+        title: 'PHP',
+        level: 6
+    }
+];
--- JS/functions.js
+++ JS/functions.js
@@ -1,72 +1,12 @@
-var user_bio = {
-    name: "José Manuel",
-    last_name: "Cornelio",
-    professional_title: "Web Developer",
-    about: "More than 6 years of experience creating amazing websites and web apps as freelancer and employee of great agencies and organizations, even the government.",
-    birth_date: "08-19-1993",
-    city: "Santo Domingo",
-    country: "Dominican Republic"
-};
-
-var jobs = [
-    {
-        title: 'Best Choice LLC',
-        monts: 14,
-        description: 'Web Developer and Project Manager'
-    },
-    {
-        title: 'Arzobispado de Sto. Dgo',
-        monts: 28,
-        description: 'Web Developer and Multimedia Designer'
-    },
-    {
-        title: 'Proceso.com.do',
-        monts: 10,
-        description: 'Web Developer'
-    },
-    {
-        title: 'DIGEPEP',
-        monts: 16,
-        description: 'WordPress Developer, Front-End Developer and Web Master'
-    }
-]
-
-var skills = [
-    {
-        title: 'HTML',
-        level: 9
-    },
-    {
-        title: 'CSS',
-        level: 7
-    },
-    {
-        title: 'JavaScript',
-        level: 8
-    },
-    {
-        title: 'PHP',
-        level: 6
-    }
-];
-
-
-
-
-
+// Site Title
+document.getElementById("site_title").innerHTML = `${user_bio.name} ${user_bio.last_name} | ${user_bio.professional_title}`;
 
 //User information output
 document.getElementById("user_name").innerHTML = `${user_bio.name} ${user_bio.last_name}`;
 document.getElementById("professional_title").innerHTML = user_bio.professional_title;
 document.getElementById("about").innerHTML = user_bio.about;
 
-jobs.forEach(job => document.getElementById("experience").innerHTML += 
-    `<span class="experience">
-        <h4>${job.title} <span>(${job.monts} months)</span></h4>
-        <p>${job.description}</p>
-    </span>`
-);
-
+//Main Skills Progress Bars
 skills.forEach(skill => document.getElementById("skills_list").innerHTML += `
     <tr>
         <td class="skill">${skill.title}</td>
@@ -77,5 +17,10 @@ skills.forEach(skill => document.getElementById("skills_list").innerHTML += `
     </tr>
 `);
 
-// Site Title
-document.getElementById("site_title").innerHTML = `${user_bio.name} ${user_bio.last_name} | ${user_bio.professional_title}`;
+//Job Experience List
+jobs.forEach(job => document.getElementById("experience").innerHTML += 
+    `<span class="experience">
+        <h4>${job.title} <span>(${job.monts} months)</span></h4>
+        <p>${job.description}</p>
+    </span>`
+);
